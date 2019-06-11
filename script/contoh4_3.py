b08f926cd59f5bd278d17e98d10bb06e76413a39
sandyherho <sandyherho@ymail.com>
2019-06-11 12:51:16 -0400

    adding contoh4_4.py

---
+++ script/contoh4_3.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plotsin(flip=1):
+    x = np.linspace(0,14,100)
+    for i in range(1,5):
+        plt.plot(x,np.sin(x + i * 0.5) * (7 - i) * flip)
+
+sns.set()
+
+sns.set_style('whitegrid')
+
+plotsin()
+
+plt.show()
+
