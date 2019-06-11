f3bb824e64914cd742a7dc096c996a2f9cd272a9
sandyherho <sandyherho@ymail.com>
2019-06-11 10:40:10 -0400

    adding contoh4_2.py

---
+++ script/contoh4_2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plotsin(flip=1):
+    x = np.linspace(0,14,100)
+    for i in range(1,5):
+        plt.plot(x,np.sin(x + i * 0.5) * (7 - i) * flip)
+
+sns.set()
+
+plotsin()
+
+plt.show()
