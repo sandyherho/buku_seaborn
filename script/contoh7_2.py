8644b3f814ebda71cebd299f6bd0132f18bc0cce
sandyherho <sandyherho@ymail.com>
2019-06-11 20:10:03 -0400

    adding contoh7_2.py

---
+++ script/contoh7_2.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = sns.load_dataset('iris')
+
+sns.jointplot(x='petal_length', y='petal_width', data=df, kind='hex')
+
+plt.show()
